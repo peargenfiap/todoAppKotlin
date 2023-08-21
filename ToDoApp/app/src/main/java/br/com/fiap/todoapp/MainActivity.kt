987a2129d3a4e7092a8764e0e7e2ee804094bf8a
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-21 20:57:48 -0300

    initial commit

---
+++ ToDoApp/app/src/main/java/br/com/fiap/todoapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package br.com.fiap.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.fiap.todoapp.databinding.ActivityMainBinding
+import br.com.fiap.todoapp.databinding.ViewFilterItemBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupFields()
+    }
+
+    private fun setupFields() {
+        TaskStatus.values().forEach {
+            val filterOption = ViewFilterItemBinding.inflate(
+                layoutInflater,
+                binding.taskFilters,
+                false
+            )
+            filterOption.filter.text = it.title
+            binding.taskFilters.addView(filterOption.root)
+
+        }
+    }
+}
